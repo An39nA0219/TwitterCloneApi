171eba291d01d3944edd42947093ba478efa3b43
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-11-01 16:02:39 +0900

    プロジェクト ファイルを追加します。

---
+++ TwitterCloneApi/Data/TwitterCloneApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TwitterCloneApi.Models;
+
+namespace TwitterCloneApi.Data
+{
+    public class TwitterCloneApiContext : DbContext
+    {
+        public TwitterCloneApiContext (DbContextOptions<TwitterCloneApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TwitterCloneApi.Models.User> User { get; set; }
+    }
+}
---
+++ TwitterCloneApi/Models/TwitterCloneContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TwitterCloneApi.Models
+{
+    public class TwitterCloneContext : DbContext
+    {
+        public TwitterCloneContext(DbContextOptions<TwitterCloneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> TodoItems { get; set; }
+    }
+}
